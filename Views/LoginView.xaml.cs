3d0e3910376e9fd29f0414d2c5d89fb5da5d2a58
ERICKABDIEL <abdiel321atzin@gmail.com>
2024-10-17 13:49:06 -0600

    Se agregaron cambios a la ventana de registrar

--- Views/LoginView.xaml.cs
+++ Views/LoginView.xaml.cs
@@ -32,46 +32,37 @@ namespace TripasDeGatoCliente.Views
 
         private void BtnLogin_Click(object sender, RoutedEventArgs e)
         {
-            // Resetear los bordes a color blanco para errores anteriores
             txtEmail.BorderBrush = new SolidColorBrush(Colors.White);
             txtPassword.BorderBrush = new SolidColorBrush(Colors.White);
 
-            // Crear el objeto LoginUser con el correo y la contraseña (hasheada)
             LoginUser userAccount = new LoginUser
             {
                 mail = txtEmail.Text,
-                password = Hasher.HashToSHA256(txtPassword.Password) // Hasheamos la contraseña antes de enviar
+                password = Hasher.HashToSHA256(txtPassword.Password) 
             };
 
-            // Verificar los campos
             if (VerifyFields())
             {
-                // Validar las credenciales con el servidor
                 if (ValidateCredentials(userAccount))
                 {
-                    // Si son válidas, navegar al menú principal
                     DisplayMainMenuView();
                 }
                 else
                 {
-                    // Mostrar mensaje de error si no coinciden las credenciales
                     DialogManager.ShowErrorMessageAlert(Properties.Resources.dialogMissmatchesCredentials);
                 }
             }
             else
             {
-                // Mostrar mensaje de error si los campos no son válidos
                 DialogManager.ShowErrorMessageAlert(Properties.Resources.dialogWrongData);
             }
         }
 
-        // Método para verificar los campos de texto (email y contraseña)
         private bool VerifyFields()
         {
             bool passwordValid = Validador.ValidatePassword(txtPassword.Password);
             bool emailValid = Validador.ValidateEmail(txtEmail.Text);
 
-            // Mostrar bordes en rojo si hay errores
             if (!passwordValid)
             {
                 txtPassword.BorderBrush = Brushes.Red;
@@ -84,7 +75,6 @@ namespace TripasDeGatoCliente.Views
             return passwordValid && emailValid;
         }
 
-        // Método para validar credenciales llamando al servidor
         public bool ValidateCredentials(LoginUser user)
         {
             bool validateResult = false;
@@ -93,18 +83,15 @@ namespace TripasDeGatoCliente.Views
 
             try
             {
-                // Crear instancia del servicio y verificar el login
                 IUserManager userManager = new UserManagerClient();
                 validationResult = userManager.verifyLogin(user);
 
-                // Validar el resultado devuelto por el servicio
                 if (validationResult == Constants.DATA_MATCHES)
                 {
-                    // Si las credenciales son válidas, obtener el perfil del usuario
-                    Profile profile = userManager.getProfile(user.mail);
+                    Profile profile = userManager.getProfile(user.mail, user.password);
                     if (profile != null)
                     {
-                        ObtainSingletonData(profile); // Guardar los datos del perfil en el singleton
+                        ObtainSingletonData(profile); 
                         validateResult = true;
                     }
                     else
@@ -126,14 +113,11 @@ namespace TripasDeGatoCliente.Views
             return validateResult;
         }
 
-        // Método para almacenar los datos del perfil en el Singleton
         private void ObtainSingletonData(Profile profile)
         {
-            // Llamamos al Singleton y creamos la instancia con los datos del perfil
             UserProfileSingleton.Instance.CreateInstance(profile);
         }
 
-        // Método para navegar al menú principal si el login es exitoso
         private void DisplayMainMenuView()
         {
             LobbyView lobbyView = new LobbyView();
@@ -145,7 +129,6 @@ namespace TripasDeGatoCliente.Views
             this.NavigationService.Navigate(registerView);
         }
 
-        // Método para mostrar la contraseña
         private void BtnTogglePassword_Checked(object sender, RoutedEventArgs e)
         {
             txtPasswordVisible.Text = txtPassword.Password;
@@ -153,7 +136,6 @@ namespace TripasDeGatoCliente.Views
             txtPassword.Visibility = Visibility.Collapsed;
         }
 
-        // Método para ocultar la contraseña
         private void BtnTogglePassword_Unchecked(object sender, RoutedEventArgs e)
         {
             txtPassword.Password = txtPasswordVisible.Text;
@@ -161,7 +143,6 @@ namespace TripasDeGatoCliente.Views
             txtPassword.Visibility = Visibility.Visible;
         }
 
-        // Actualiza el icono de visibilidad de la contraseña
         private void UpdatePasswordVisibilityIcon()
         {
             if (txtPassword.Password.Length > 0)
